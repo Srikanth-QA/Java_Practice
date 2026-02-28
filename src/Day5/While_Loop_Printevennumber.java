16ddf9000ba7ffc0c517400dafd5cc54428836cd
Srikanth-QA <srikanthss716@gmail.com>
2026-02-28 21:35:42 +0530

    new

---
+++ src/Day5/While_Loop_Printevennumber.java
@@ -0,0 +1,15 @@
+package Day5;
+
+public class While_Loop_Printevennumber {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i=1;
+		while(i<=10)
+		{ 
+			System.out.println(i);
+			i+=2;
+		}
+	}
+
+}
---
+++ src/Day5/Whileloop_print_numbers.java
@@ -0,0 +1,13 @@
+package Day5;
+
+public class Whileloop_print_numbers {
+	public static void main(String[] args)
+	{
+int i=1;
+while(i<=10)
+{ 
+	System.out.println(i);
+	i++;
+}
+}
+}
