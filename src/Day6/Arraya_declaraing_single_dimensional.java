87ae6555f09e1e3951182a2973a9d3b9f0505c1c
Srikanth-QA <srikanthss716@gmail.com>
2026-03-06 23:33:52 +0530

    hn

---
+++ src/Day6/Arraya_declaraing_single_dimensional.java
@@ -0,0 +1,25 @@
+package Day6;
+
+public class Arraya_declaraing_single_dimensional {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//Approach 1
+		int a[]=new int [5];
+		a[0]=100;
+		a[1]=200;
+		a[2]=300;
+		a[3]=400;
+		a[4]=500;
+		
+		//find the length of array
+		System.out.println(a.length);
+		
+		//read single value
+		System.out.println(a[4]);
+		
+		
+		
+	}
+
+}
