063e284b268c004dec8bc391c5a05e2d39146eaa
Srikanth-QA <srikanthss716@gmail.com>
2026-03-11 21:59:34 +0530

    qa

--- src/Day8/Split_the_string_into_multiple_parts_based_on_delemiter.java
+++ src/Day8/Split_the_string_into_multiple_parts_based_on_delemiter.java
@@ -1,11 +1,14 @@
 package Day8;
 
+import java.util.Arrays;
+
 public class Split_the_string_into_multiple_parts_based_on_delemiter {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 String s="QA@gmail.com";
 String a[]= s.split("@");
+System.out.println(Arrays.toString(a));
 System.out.println(a[0]);
 System.out.println(a[1]);
 	}
