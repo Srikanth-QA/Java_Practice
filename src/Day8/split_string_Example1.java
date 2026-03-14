77563395d166a9042d90bfd21664524b49172497
Srikanth-QA <srikanthss716@gmail.com>
2026-03-14 14:24:30 +0530

    Create split_string_Example1.java

---
+++ src/Day8/split_string_Example1.java
@@ -0,0 +1,12 @@
+package Day8;
+
+public class split_string_Example1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+String amount = "$15, 20, 55";
+System.out.println(amount.replace("$", " "));//remove $ symbol
+System.out.println(amount.replace("$"," ").replace(","," ")); //remove , symbol
+	}
+
+}
