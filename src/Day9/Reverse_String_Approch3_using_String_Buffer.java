9e1dcbd3bc94c7832f8c2b9fcdc9ab4fcf44d336
Srikanth-QA <srikanthss716@gmail.com>
2026-03-15 08:47:36 +0530

    Reverse_String_Approch3&4

---
+++ src/Day9/Reverse_String_Approch3_using_String_Buffer.java
@@ -0,0 +1,12 @@
+package Day9;
+
+public class Reverse_String_Approch3_using_String_Buffer {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//String s="SRIKANTH";
+StringBuffer s=new StringBuffer("SRIKANTH");
+System.out.println("Reverse String is: " + s.reverse());
+	}
+
+}
---
+++ src/Day9/Reverse_String_Approch4_using_String_Builder.java
@@ -0,0 +1,11 @@
+package Day9;
+
+public class Reverse_String_Approch4_using_String_Builder {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		StringBuilder s=new StringBuilder("SRIKANTH");
+		System.out.println("Reverse String is: " + s.reverse());
+	}
+
+}
