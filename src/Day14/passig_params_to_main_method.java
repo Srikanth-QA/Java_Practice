79f938e4d08cb9a648387f4376794e22e01f3884
Srikanth-QA <srikanthss716@gmail.com>
2026-03-22 00:47:21 +0530

    pass params to main methods

---
+++ src/Day14/passig_params_to_main_method.java
@@ -0,0 +1,14 @@
+package Day14;
+
+public class passig_params_to_main_method {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println(args.length);
+for(String value:args)
+{
+	System.out.println(value);
+}
+	}
+
+}
