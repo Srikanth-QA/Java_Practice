ed1bcf5e5cd3d639be00322915cae8bcc49e749b
Srikanth-QA <srikanthss716@gmail.com>
2026-03-21 14:09:24 +0530

    method_studebt data

---
+++ src/day11/Class_Student_Main.java
@@ -0,0 +1,16 @@
+package day11;
+
+public class Class_Student_Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		class_Student stu =new class_Student();
+		//using object refernce variable
+		stu.sid= 100;
+		stu.Sname = "Srikanth";
+		stu.Grade = 'A';
+		stu.PrintStudentData();
+	
+	}
+
+}
---
+++ src/day11/class_Student.java
@@ -0,0 +1,32 @@
+package day11;
+
+public class class_Student {
+int  sid;
+String Sname;
+char Grade;
+void PrintStudentData()
+{
+	System.out.println(sid+" "+Sname+" "+Grade);
+}
+
+void setStudentData(int id, String name, char gr)
+{
+	sid =id;
+	Sname = name;
+	Grade = gr;
+}
+
+void Student(int id, String name, char gr)
+{
+	sid =id;
+	Sname = name;
+	Grade = gr;
+}
+
+void Student1(int id, String name, char gr)
+{
+	sid =id;
+	Sname = name;
+	Grade = gr;
+}
+}
